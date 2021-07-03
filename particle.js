caf4202e5dc65f8e49354abb7ea5d659e3752323
Shruthilayaa <77278761+Shruthilayaa@users.noreply.github.com>
2021-07-03 08:54:57 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,15 @@
+class particle{
+    constructor(x,y){
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.body = Bodies.circle (x,y,this.r,options) 
+        this.color=color(random(0,255),random(0,255),random(0,255))
+        World.add(world,this.body);
+    }
+    //display(){
+
+   // }
+}
